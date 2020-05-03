cc3e90a81351503f5acfb9029acb9fe1b987f627
Adrian Mucha <kestarumper8@gmail.com>
2020-05-03 13:30:05 +0200

    Sprawko Zad 2 MO.

--- I/MetodyOptymalizacji/l2/zad2.jl
+++ I/MetodyOptymalizacji/l2/zad2.jl
@@ -10,7 +10,7 @@ using GLPK
 struct SubProgram
     name::Tuple{Int, Int}
     r::Int
-    t::Float64
+    t::Int
 end
 
 function zad2(P::Array{SubProgram, 2}, I::Vector{Int}, M::Int, verbose = true)
@@ -28,20 +28,18 @@ function zad2(P::Array{SubProgram, 2}, I::Vector{Int}, M::Int, verbose = true)
 	Programs = 1:m
 	SubPrograms = 1:n
 
-	println("Programy range", Programs)
-	println("SubProgramy range", SubPrograms)
-
     # Które podprogramy wybrać do obliczenia funkcji
 	@variable(model, x[Programs, SubPrograms], Bin)
 	
 	# minimalizacja sumy czasu wykonania
 	@objective(model, Min, sum(P[i,j].t * x[i,j] for i in Programs, j in SubPrograms))
 	
-	# dostęp do każdej cechy w conajmniej jednym wybranym serwerze
+	# wybrane podprogramy nie przekraczają łącznie ograniczeń pamięci
     @constraint(model, sum(P[i,j].r * x[i,j] for i in Programs, j in SubPrograms) <= M)
-
-    for i in Programs
-        @constraint(model, sum(x[i,j] for j in SubPrograms) >= 1)
+    
+    # uwzględniaj tylko zlecone funkcje (jeden podprogram na zleconą funkcję)
+    for i in I
+        @constraint(model, sum(x[i,j] for j in SubPrograms) == 1)
     end
 	
 	print(model) # drukuj model
@@ -71,7 +69,7 @@ I = [1,3]
 
 function subprogramFactory(i::Int, j::Int)
     mem = i * j
-    time = 1 / mem * 10
+    time = Int(floor(100 / mem))
     return SubProgram((i, j), mem, time)
 end
 
@@ -80,21 +78,27 @@ P = [subprogramFactory(i, j) for i in 1:m, j in 1:n]
 
 (status, fcelu, subprograms) = zad2(P, I, M, true)
 
-function listSubprograms(xs)
+function getSubprograms(xs)
     m, n = size(xs)
+    subprograms = []
     for i in 1:m
         for j in 1:n
             if xs[i, j] > 0
-                println("P", i, j)
+                push!(subprograms, (i, j))
             end
         end
     end
+    return subprograms
 end
 
 if status == MOI.OPTIMAL
-    println("funkcja celu: ", fcelu)
-    println("Które serwery: ", subprograms)
-    listSubprograms(subprograms)
+    subprogramIndexes = getSubprograms(subprograms)
+    subprogramInstances = map(i -> P[i[1], i[2]], subprogramIndexes)
+    memTotal = reduce(+, map(p -> p.r, subprogramInstances); init=0)
+    println("Wybrane podprogramy: ", subprograms)
+    println("Program P, sklada sie z nastepujacych funkcji: ", subprogramInstances)
+    println("Zużyta pamięć: ", memTotal, "b")
+    println("Czas wykonania programu P: ", fcelu, "ms")
 else
     println("Status: ", status)
 end

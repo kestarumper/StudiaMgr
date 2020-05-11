ecd318056e5ca6929805374ab0e99f4b849043d8
Adrian Mucha <kestarumper8@gmail.com>
2020-05-11 12:11:30 +0200

    Bin search find n.

--- I/AnalizaAlgorytmow/l4/find_n.py
+++ I/AnalizaAlgorytmow/l4/find_n.py
@@ -2,36 +2,41 @@ from main import experiment
 import numpy as np
 
 
-def find_n(upper_bound, q):
+def binary_search_n(upper_bound, q):
+    """
+    Finds the smallest `n` that provides sufficient security of success `upper_bound`
+    when adversary has `q = 1 - p` computational power (0 < q < 0.5).
+    """
     samples = 1000
-    n = 24
-    epsilon_rate = 0.001
-    max_iterations = 15
+    epsilon = 0.001
+    low = 1
+    high = low
 
-    best_n = n
-    best_err = 1
-    for i in range(max_iterations):
-        rate = experiment(n, q, samples)
-        print(f"\tn={n} | rate={rate}")
+    while True:
+        rate = experiment(high, q, samples)
+        print(f"\tn={high} | rate={rate:.4f}")
+        if rate < upper_bound:
+            break
+        low = high
+        high += high
 
-        diff_n = n // 2
+    while high >= low:
+        mid = low + ((high - low) // 2)
+        rate = experiment(mid, q, samples)
+
         diff_rate = upper_bound - rate
         absolute_error = abs(diff_rate)
 
-        if absolute_error < best_err:
-            best_n = n
-            best_err = absolute_error
+        print(f"\tn={mid} | rate={rate:.3f} | err={absolute_error:.4f}")
 
-        if (diff_n == 0) or (absolute_error < epsilon_rate):
-            print(f"\t\tINFO:\tFound after {i} iterations.")
-            return n
-        if diff_rate > 0:
-            n -= diff_n
+        if rate < upper_bound:
+            high = mid - 1
         else:
-            n += diff_n
-        
-    print(f"\t\tWARN:Max iterations exceeded.")
-    return best_n
+            if absolute_error < epsilon:
+                return mid
+            low = mid + 1
+
+    return low
 
 
 if __name__ == "__main__":
@@ -42,6 +47,6 @@ if __name__ == "__main__":
         with open(fname, 'w') as file:
             for q in np.linspace(0.01, 0.49, 49):
                 q = round(q, 2)
-                n = find_n(upper_bound, q)
+                n = binary_search_n(upper_bound, q)
                 print(f"\t\tq={q} | n={n}")
                 file.write(f"{q},{n}\n")

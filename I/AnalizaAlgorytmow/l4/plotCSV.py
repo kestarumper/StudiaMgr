b72d23eb76117fdb71ca8b52e475ac605a7e1851
Adrian Mucha <kestarumper8@gmail.com>
2020-05-11 19:57:38 +0200

    Plot nakamoto + grunspan.

--- I/AnalizaAlgorytmow/l4/plotCSV.py
+++ I/AnalizaAlgorytmow/l4/plotCSV.py
@@ -1,8 +1,45 @@
-import csv
+from math import exp, factorial, comb
+from pathlib import Path
 import argparse
+import csv
 import matplotlib.pyplot as plt
 import numpy as np
-from pathlib import Path
+import re
+
+
+def nakamoto(n, q):
+    result = 1
+    p = 1 - q
+    lambd = n * (q / p)
+    for k in range(n):
+        result -= exp(-lambd) * ((lambd ** k) / factorial(k)) * \
+            (1 - ((q / p) ** (n - k)))
+    return result
+
+
+def grunspan(n, q):
+    result = 1
+    p = 1 - q
+    for k in range(n):
+        result -= ((p ** n) * (q ** k) - (q ** n)
+                   * (p ** k)) * comb(k + n - 1, k)
+    return result
+
+
+def plot_nakamoto_and_grunspan(n):
+    xs = []
+    ys = []
+    yss = []
+    for q in np.linspace(0.01, 0.49, 49):
+        q = round(q, 2)
+        P = nakamoto(n, q)
+        P2 = grunspan(n, q)
+        xs.append(q)
+        ys.append(P)
+        yss.append(P2)
+    plt.plot(xs, ys, c="blue", label="nakamoto")
+    plt.plot(xs, yss, c="green", label="grunspan")
+    return xs, ys
 
 
 def main():
@@ -10,9 +47,9 @@ def main():
     parser.add_argument("-t", "--title",
                         help="Title")
     parser.add_argument("-x", "--xlabel", default="q",
-                        help="Title")
+                        help="Arguments label")
     parser.add_argument("-y", "--ylabel", default="adversary success rate",
-                        help="Title")
+                        help="Values label")
     parser.add_argument("-f", "--file",
                         help="File to be processed")
     args = parser.parse_args()
@@ -41,7 +78,14 @@ def main():
 
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.plot(x, y, c="red")
+        plt.plot(x, y, c="red", label="experiment")
+
+        n = re.search("n_(\d+)_", Path(args.file).stem)
+        if n:
+            n = int(n.group(1))
+            plot_nakamoto_and_grunspan(n)
+
+        plt.legend()
         plt.savefig(f"{Path(args.file).stem}.png")
 
 

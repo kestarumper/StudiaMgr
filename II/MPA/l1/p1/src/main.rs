a8d54bc362fe91dc7d6ffc6a4511a57e10ad37a0
Adrian Mucha <kestarumper8@gmail.com>
2020-10-18 18:28:50 +0200

    MPA L1P1 Experiments to csv.

--- II/MPA/l1/p1/src/main.rs
+++ II/MPA/l1/p1/src/main.rs
@@ -5,11 +5,22 @@ use merge_sort::mergesort;
 use quick_sort::quicksort;
 use rand::Rng;
 
-pub fn make_array(capacity: usize, min: i32, max: i32) -> Vec<i32> {
+use csv::Writer;
+use std::error::Error;
+
+fn write_to_csv(vec: &Vec<Experiment>, path: &str) -> Result<(), Box<Error>> {
+    let mut wtr = Writer::from_path(path)?;
+    for record in vec {
+        wtr.serialize(record)?;
+    }
+    wtr.flush()?;
+    Ok(())
+}
+
+pub fn make_array(capacity: usize) -> Vec<i32> {
     let mut rng = rand::thread_rng();
     return ::std::iter::repeat(())
         .map(|()| rng.gen::<i32>())
-        .map(|num| (num % max + min) % max)
         .take(capacity)
         .collect::<Vec<i32>>();
 }
@@ -20,18 +31,48 @@ fn assert_sorted(vec: &[i32]) {
     }
 }
 
+type Experiment = (usize, Vec<usize>);
+
+fn experiments(
+    nmin: usize,
+    nmax: usize,
+    step: usize,
+    repeat: usize,
+    func: fn(&mut [i32]) -> usize,
+) -> Vec<Experiment> {
+    let mut result: Vec<Experiment> = Vec::new();
+    for n in (nmin..nmax).step_by(step) {
+        let compares = experiment(n, repeat, func);
+        println!("n={:?}", n);
+        result.push((n, compares));
+    }
+    return result;
+}
+
+fn experiment(n: usize, repeat: usize, func: fn(&mut [i32]) -> usize) -> Vec<usize> {
+    let mut result: Vec<usize> = Vec::new();
+    for i in 0..repeat {
+        let arr: &mut [i32] = &mut make_array(n)[..];
+        let compares = func(arr);
+        assert_sorted(arr);
+        result.push(compares);
+    }
+    return result;
+}
+
 pub fn main() {
-    let arr: &mut [i32] = &mut make_array(usize::pow(10, 1), -1000, 1000)[..];
-    let qc_compares = quicksort(arr);
+    let nmin = 100;
+    let nmax = usize::pow(10, 4) + 1;
+    let step = 100;
+    let repeat = 1000;
+    let result = &experiments(nmin, nmax, step, repeat, quicksort);
+    // let result = &experiments(nmin, nmax, step, repeat, mergesort);
 
-    assert_sorted(arr);
-
-    println!("Total QS compares: {}", qc_compares);
-
-    let arr2: &mut [i32] = &mut make_array(usize::pow(10, 1), -1000, 1000)[..];
-    let ms_compares = mergesort(arr2);
-
-    assert_sorted(arr2);
-
-    println!("Total MS compares: {}", ms_compares);
+    if let Err(err) = write_to_csv(&result, "experiment.csv") {
+        println!("There was an error while saving to csv file");
+        println!("{}", err);
+        for row in result {
+            println!("{:?}", row);
+        }
+    }
 }

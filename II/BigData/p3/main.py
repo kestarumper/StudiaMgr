556064d6618b6a1448274ec7b17be6ed5aa34c6e
Adrian Mucha <kestarumper8@gmail.com>
2020-10-31 12:49:59 +0100

    BD LAB Spark p1.

---
+++ II/BigData/p3/main.py
@@ -0,0 +1,34 @@
+from pyspark import SparkContext, SparkConf
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+import csv
+
+tokenizer = RegexpTokenizer(r'\w+')
+fname = "lotr.txt"  # Should be some file on your system
+conf = SparkConf().setAppName("Book").setMaster('local')
+sc = SparkContext(conf=conf)
+
+def lineToWords(line):
+    return tokenizer.tokenize(line)
+
+def filterStopWords(word):
+    return (word not in stopWords) and (len(word) > 0)
+
+def writeCSVToFile(data, fname):
+    with open(fname,'w') as out:
+        csv_out=csv.writer(out)
+        csv_out.writerow(['weight','word'])
+        csv_out.writerows(data)
+
+stopWords = stopwords.words("english")
+
+lines = sc.textFile(fname)
+words = lines.flatMap(lineToWords)
+filtered = words.filter(filterStopWords)
+pairs = filtered.map(lambda w: (w.lower(), 1))
+grouped = pairs.reduceByKey(lambda a, b: a + b)
+sorted = grouped.sortBy(lambda x: -x[1])
+
+writeCSVToFile(sorted.map(lambda x: (x[1], x[0])).take(500), "wordcloud.csv")
+
+sc.stop()

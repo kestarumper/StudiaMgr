8e9703972c4950a16a5bfbb2432fdda089a59ed9
Adrian Mucha <kestarumper8@gmail.com>
2020-11-16 18:34:47 +0100

    BD p29 reinvented to aggregate.

--- II/BigData/p29/main.py
+++ II/BigData/p29/main.py
@@ -5,35 +5,40 @@ from operator import add
 conf = SparkConf().setAppName("Stats").setMaster('local')
 sc = SparkContext(conf=conf)
 
+def seq_operator(acc, value):
+    min_v, max_v, count, avg_v, distinct = acc
 
-def mean(x, y):
-    (a, m) = x
-    (b, n) = y
-    N = m + n
-    return (((a * m) / N) + ((b * n) / N), N)
+    count += 1
 
+    min_v = min(min_v, value)
+    max_v = max(max_v, value)
+
+    avg_v = ((count - 1) / count) * avg_v + (value / count)
+
+    if not distinct.get(value):
+        distinct[value] = True
+    
+    return (min_v, max_v, count, avg_v, distinct)
+
+def comb_operator(x, y):
+    min_v = min(x[0], y[0])
+    max_v = max(x[1], y[1])
+    count = x[2] + y[2]
+    avg_v = (x[2] / count) * x[3] + (y[2] / count) * y[3]
+    distinct = {**x[4], **y[4]}
+    return (min_v, max_v, count, avg_v, distinct)
 
 data = np.random.randint(0, 2_000_000, size=1_000_000)
 distData = sc.parallelize(data)
 
-gmin = distData.reduce(min)
-gmax = distData.reduce(max)
-N = distData.count()
-avg = distData.map(lambda x: (x, 1)).reduce(mean)[0]
-distinct = distData.distinct()
-numOfElements = distData \
-    .map(lambda x: (x, 1)) \
-    .reduceByKey(add) \
-    .filter(lambda y: y[1] == 1) \
-    .count()
+labels = ["min", "max", "count", "avg", "distinct", "distinct_num"]
+zero_value = (data[0], data[0], 0, 0, {})
 
-geometricMean = distData.reduce(lambda a, b: a ** (1./N) * b ** (1./N))
-harmonicMean = N / distData.map(lambda x: 1/x).sum()
+result = distData.aggregate(zero_value, seq_operator, comb_operator)
+result = result[:-1] + (len(result[-1]),)
 
-print({"min": gmin, "max": gmax, "n": N, "avg": avg,
-       "G": geometricMean, "H": harmonicMean})
-print(np.mean(data))
-print(distinct.take(100))
-print(numOfElements)
+verbose = zip(labels, result)
+
+print({ k: v for k, v in verbose })
 
 sc.stop()
